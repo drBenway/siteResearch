da2f549b2cc3749168c056ba2f9c17ed1fab26e6
westworld <noone@westworld.be>
2012-10-14 15:08:01 +0200

    first working version
    first working version. Updated the code to use Namespaces and did some
    extra cleanup. New version will probably use components from Symfony
    since I like the dom  parser better and want a clean way for DI and
    configuration files.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once("/Applications/MAMP/htdocs/siteResearch/siteResearch/Crawler/config/SplClassLoader.php");
+require_once(__DIR__."../Crawler/config/SplClassLoader.php");
 $loader = new SplClassLoader('siteResearch', '/Applications/MAMP/htdocs/siteResearch');
 $loader->register();
 ?>
